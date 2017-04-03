0f35b177a158bf94abf4d9f772e6c05096a2bcf2
Steven de Salas <steven@desalasworks.com>
2017-04-03 23:47:10 +1000

    Wizard Steps Proof of Concept

--- src/components/wizard.js
+++ src/components/wizard.js
@@ -19,12 +19,12 @@ function WizardStep(props) {
     }
 
     return <div className='wizard-step' style={{ ...props.style }}>
-                <a name='close' className='close' onClick={close}>x</a>
+                <a name='close' className='close' onClick={ props.close }>x</a>
                 <h2>{props.name}</h2>
                 <div>{ props.children }</div>
                 <div className='wizard-footer'>
                     <label for='hidePermanently'>
-                        <input id='hidePermanently' type='checkbox'/> 
+                        <input id='hidePermanently' type='checkbox'/>
                         Do not show this again
                     </label>
                     <button className="btn-next btn btn-primary" onClick={next}>Next</button>
@@ -39,31 +39,46 @@ function WizardStep(props) {
  * @param {Object} props Component properties.
  */
 class Wizard extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        visible: true
+      };
+    }
+
+    close (e) {
+        console.log('Wizard.close()');
+        this.setState({ visible: false });
+    }
+
     /**
      * Render the component.
      * @return {String}
      */
     render() {
+
         return (
+            this.state.visible ?
             <div className='wizard'>
-                <WizardStep name='Welcome to Laserweb' style={{ display: 'block' }}>
+                <WizardStep name='Welcome to Laserweb' style={{ display: 'block' }} close={ this.close.bind(this) }>
                     <div className="row" style={{ textAlign: 'center', marginTop: '40px'}}>
                       <div className="col-xs-6">
                         <a className='btn btn-default btn-lg'>
-                          <p><span className="glyphicon glyphicon-folder-open" aria-hidden="true"></span></p> 
+                          <p><span className="glyphicon glyphicon-folder-open" aria-hidden="true"></span></p>
                           Open Project
                         </a>
                       </div>
                       <div className="col-xs-6">
                         <a className='btn btn-default btn-lg'>
-                        <p><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></p> 
+                        <p><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></p>
                         Create New
                         </a>
                       </div>
                     </div>
                 </WizardStep>
                 <WizardStep name='Step2'>Wizard Step 2</WizardStep>
-            </div>
+            </div> : null
         )
     }
 }
